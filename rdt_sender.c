ec9c8e8f7ca0eb2ff721db29151175b2ed2ced5e
tytyhensley <tkh265@nyu.edu>
2020-04-30 13:28:13 +0400

    Task 1

---
+++ rdt_sender.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <time.h>
+#include <assert.h>
+
+#include"packet.h"
+#include"common.h"
+
+#define STDIN_FD    0
+#define RETRY  120 //milli second 
+
+int next_seqno=0;
+int send_base=0;
+int window_size = 10;
+int sockfd, serverlen;
+struct sockaddr_in serveraddr;
+struct itimerval timer; 
+tcp_packet *sndpkt;
+tcp_packet *recvpkt;
+tcp_packet *window_pkt[10]; // a pointer array of all apckets in the current window
+sigset_t sigmask;       
+  
+
+void resend_packets(int sig)
+{
+    if (sig == SIGALRM)
+    {
+        //Resend all packets range between 
+        //sendBase and nextSeqNum
+        VLOG(INFO, "Timeout happend");
+        for (int i = 0; i<window_size; i++){
+            if(sendto(sockfd, window_pkt[i], TCP_HDR_SIZE + get_data_size(window_pkt[i]), 0, 
+                        ( const struct sockaddr *)&serveraddr, serverlen) < 0)
+            {
+                error("sendto");
+            }
+        }
+    }
+}
+
+
+void start_timer()
+{
+    sigprocmask(SIG_UNBLOCK, &sigmask, NULL);
+    setitimer(ITIMER_REAL, &timer, NULL);
+}
+
+
+void stop_timer()
+{
+    sigprocmask(SIG_BLOCK, &sigmask, NULL);
+}
+
+
+/*
+ * init_timer: Initialize timeer
+ * delay: delay in milli seconds
+ * sig_handler: signal handler function for resending unacknoledge packets
+ */
+void init_timer(int delay, void (*sig_handler)(int)) 
+{
+    signal(SIGALRM, resend_packets);
+    timer.it_interval.tv_sec = delay / 1000;    // sets an interval of the timer
+    timer.it_interval.tv_usec = (delay % 1000) * 1000;  
+    timer.it_value.tv_sec = delay / 1000;       // sets an initial value
+    timer.it_value.tv_usec = (delay % 1000) * 1000;
+
+    sigemptyset(&sigmask);
+    sigaddset(&sigmask, SIGALRM);
+}
+
+
+int main (int argc, char **argv)
+{
+    int portno, len;
+    int next_seqno;
+    char *hostname;
+    char buffer[DATA_SIZE];
+    int start = 0;
+    FILE *fp;
+
+    /* check command line arguments */
+    if (argc != 4) {
+        fprintf(stderr,"usage: %s <hostname> <port> <FILE>\n", argv[0]);
+        exit(0);
+    }
+    hostname = argv[1];
+    portno = atoi(argv[2]);
+    fp = fopen(argv[3], "r");
+    if (fp == NULL) {
+        error(argv[3]);
+    }
+
+    /* socket: create the socket */
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+
+    /* initialize server server details */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serverlen = sizeof(serveraddr);
+
+    /* covert host into network byte order */
+    if (inet_aton(hostname, &serveraddr.sin_addr) == 0) {
+        fprintf(stderr,"ERROR, invalid host %s\n", hostname);
+        exit(0);
+    }
+
+    /* build the server's Internet address */
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(portno);
+
+    assert(MSS_SIZE - TCP_HDR_SIZE > 0);
+
+    //Stop and wait protocol
+
+    init_timer(RETRY, resend_packets);
+    next_seqno = 0;
+    while (1)
+    {
+        //checks if the this the beginning and proceeds to create the initil window size fo 10 packets
+        if (start==0){
+            start = 1;
+            for (int i = 0; i<window_size; i++){
+                len = fread(buffer, 1, DATA_SIZE, fp);
+                if ( len <= 0)//checks if we've  reached the end of the file and breaks out of the for loop
+                {
+                    VLOG(INFO, "End Of File has been reached");
+                    sndpkt = make_packet(0);
+                    sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
+                            (const struct sockaddr *)&serveraddr, serverlen);
+                    break;
+                }
+                send_base = next_seqno;
+                next_seqno = send_base + len;//creates the next sequence number 
+                sndpkt = make_packet(len);
+                memcpy(sndpkt->data, buffer, len);
+                sndpkt->hdr.seqno = send_base;
+
+                window_pkt[i] = sndpkt;//adds the current send packet to the ith position in the window
+
+                VLOG(DEBUG, "Sending packet %d to %s",send_base, inet_ntoa(serveraddr.sin_addr));
+                if(sendto(sockfd, sndpkt, TCP_HDR_SIZE + get_data_size(sndpkt), 0, 
+                            ( const struct sockaddr *)&serveraddr, serverlen) < 0)
+                {
+                    error("sendto");
+                }
+            }
+        }
+        do {
+            start_timer();
+            //ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
+            //struct sockaddr *src_addr, socklen_t *addrlen);
+
+            if(recvfrom(sockfd, buffer, MSS_SIZE, 0,
+                        (struct sockaddr *) &serveraddr, (socklen_t *)&serverlen) < 0)
+            {
+                error("recvfrom");
+            }
+
+            recvpkt = (tcp_packet *)buffer;
+            //printf("%d \n", get_data_size(recvpkt));
+            assert(get_data_size(recvpkt) <= DATA_SIZE);
+            stop_timer();
+
+            free(window_pkt[0]);//deallocates the first packet in the window and frees that space in memory
+
+            for(int y = 0; y<9; y++){ //moves the window to the next 10 packets
+                window_pkt[y]=window_pkt[y+1];
+            }
+
+            len = fread(buffer, 1, DATA_SIZE, fp);
+            if ( len <= 0)
+            {
+                VLOG(INFO, "End Of File has been reached");
+                sndpkt = make_packet(0);
+                sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
+                        (const struct sockaddr *)&serveraddr, serverlen);
+                if(window_pkt[0] == window_pkt[1]){//when all the packets in thw window have been acked leave loop else continue 
+                    break;
+                }
+                continue;
+            }
+            send_base = next_seqno;
+            next_seqno = send_base + len;
+            sndpkt = make_packet(len);
+            memcpy(sndpkt->data, buffer, len);
+            sndpkt->hdr.seqno = send_base;
+
+            window_pkt[9] = sndpkt;//add the latest packet to be sent to the last position in the window
+
+            VLOG(DEBUG, "Sending packet %d to %s", 
+                send_base, inet_ntoa(serveraddr.sin_addr));
+            /*
+             * If the sendto is called for the first time, the system will
+             * will assign a random port number so that server can send its
+             * response to the src port.
+             */
+            if(sendto(sockfd, sndpkt, TCP_HDR_SIZE + get_data_size(sndpkt), 0, 
+                        ( const struct sockaddr *)&serveraddr, serverlen) < 0)
+            {
+                error("sendto");
+            }
+        
+        }while(recvpkt->hdr.ackno != window_pkt[1]->hdr.seqno);
+        break;
+    }
+    return 0;
+
+}
+
+
+
