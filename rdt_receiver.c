925f7d695f634844ba9e3feb96cbe646d7b12bd3
tytyhensley <tkh265@nyu.edu>
2020-05-17 00:01:00 +0400

    final

--- rdt_receiver.c
+++ rdt_receiver.c
@@ -6,12 +6,20 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <signal.h>
 #include <sys/time.h>
+#include <time.h>
 #include <assert.h>
+#include <math.h>
+#include <stdbool.h> 
 
 #include "common.h"
 #include "packet.h"
 
+#define STDIN_FD    0
+#define RETRY  500 //milli second 
+
+int check_buffer ();
 
 /*
  * You ar required to change the implementation to support
@@ -21,6 +29,48 @@
  */
 tcp_packet *recvpkt;
 tcp_packet *sndpkt;
+tcp_packet *packet_buffer[256];
+int seqno=0, start=0, count=0, end=0;
+FILE *fp;
+struct itimerval timer;
+sigset_t sigmask;
+
+void close_server(int signum){
+	VLOG(INFO, "End Of File has been reached");
+    fclose(fp);
+    exit(0);
+}
+
+void start_timer()
+{
+    sigprocmask(SIG_UNBLOCK, &sigmask, NULL);
+    setitimer(ITIMER_REAL, &timer, NULL);
+}
+
+
+void stop_timer()
+{
+    sigprocmask(SIG_BLOCK, &sigmask, NULL);
+}
+
+
+/*
+ * init_timer: Initialize timeer
+ * delay: delay in milli seconds
+ * sig_handler: signal handler function for resending unacknoledge packets
+ */
+void init_timer(int delay, void (*sig_handler)(int)) 
+{
+    signal(SIGALRM, close_server);
+    timer.it_interval.tv_sec = delay / 1000;    // sets an interval of the timer
+    timer.it_interval.tv_usec = (delay % 1000) * 1000;  
+    timer.it_value.tv_sec = delay / 1000;       // sets an initial value
+    timer.it_value.tv_usec = (delay % 1000) * 1000;
+
+    sigemptyset(&sigmask);
+    sigaddset(&sigmask, SIGALRM);
+}
+
 
 int main(int argc, char **argv) {
     int sockfd; /* socket */
@@ -29,10 +79,8 @@ int main(int argc, char **argv) {
     struct sockaddr_in serveraddr; /* server's addr */
     struct sockaddr_in clientaddr; /* client addr */
     int optval; /* flag value for setsockopt */
-    FILE *fp;
     char buffer[MSS_SIZE];
     struct timeval tp;
-    int seqno=0;
 
     /* 
      * check command line arguments 
@@ -78,48 +126,93 @@ int main(int argc, char **argv) {
     if (bind(sockfd, (struct sockaddr *) &serveraddr, 
                 sizeof(serveraddr)) < 0) 
         error("ERROR on binding");
-
     /* 
      * main loop: wait for a datagram, then echo it
      */
     VLOG(DEBUG, "epoch time, bytes received, sequence number");
 
     clientlen = sizeof(clientaddr);
+
+    init_timer(RETRY, close_server);
     while (1) {
         /*
          * recvfrom: receive a UDP datagram from a client
          */
         //VLOG(DEBUG, "waiting from server \n");
+        if (end ==1 &&  count==0){
+        	start_timer();
+        }
         if (recvfrom(sockfd, buffer, MSS_SIZE, 0,
                 (struct sockaddr *) &clientaddr, (socklen_t *)&clientlen) < 0) {
             error("ERROR in recvfrom");
         }
         recvpkt = (tcp_packet *) buffer;
         assert(get_data_size(recvpkt) <= DATA_SIZE);
-        if ( recvpkt->hdr.data_size == 0) {
-            VLOG(INFO, "End Of File has been reached");
-            fclose(fp);
-            break;
+        if (end ==1 &&  count==0){
+        	stop_timer();
+        };
+        if (recvpkt->hdr.data_size == 0) {
+        	end = 1;
+        	goto LOOP;
         }
         /* 
          * sendto: ACK back to the client 
          */
 
         if (recvpkt->hdr.seqno == seqno){ //checks if the packet received is the expected one, if yes then write to file, if not ignore
-            gettimeofday(&tp, NULL);
-            VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno);
-            fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
-            fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
-            sndpkt = make_packet(0);
-            sndpkt->hdr.ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
-            sndpkt->hdr.ctr_flags = ACK;
-            if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
-                    (struct sockaddr *) &clientaddr, clientlen) < 0) {
-                error("ERROR in sendto");
-            }
-            seqno += recvpkt->hdr.data_size;//changes sequence number to the next expected value
-        }
-    }
+        	while(1){
+        	    gettimeofday(&tp, NULL);
+	            VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno);
+	            fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
+	            fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
 
+	            seqno += recvpkt->hdr.data_size;//changes sequence number to the next expected value
+	            LOOP: if (check_buffer()== 0){
+	             	continue;
+	             }else{
+	             	break;
+	             }
+        	}
+            
+        }else if (recvpkt->hdr.seqno > seqno) {
+        	packet_buffer[count] = recvpkt;
+        	count++;
+        	start = 1;
+        }
+        sndpkt = make_packet(0);
+        sndpkt->hdr.ackno = seqno;
+        sndpkt->hdr.ctr_flags = ACK;
+        if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
+                (struct sockaddr *) &clientaddr, clientlen) < 0) {
+            error("ERROR in sendto");
+        } 
+        printf("ACK: %d\n",seqno);      
+    }
     return 0;
+}
+
+int check_buffer (){
+	int num=0;
+	if (start==0){
+		return 1;
+	}
+	if (count == 0){
+		return 1;
+	}
+	for (int i = 0; i<count; i++){
+		if (packet_buffer[i]->hdr.seqno == seqno){
+			recvpkt=packet_buffer[i];
+			num = i+1;
+			break;
+		}
+	}
+	if (num == 0){
+		return 1;
+	}
+	for(int i=num-1; i<count; i++){
+		packet_buffer[i]=packet_buffer[i+1];
+	}
+	count--;
+	return 0;
+	
 }
--- rdt_sender.c
+++ rdt_sender.c
@@ -17,28 +17,29 @@
 #include"common.h"
 
 #define STDIN_FD    0
-#define RETRY  120 //milli second 
+#define RETRY  70 //milli second 
 #define MAX(a,b) ((a > b) ? a : b )
+#define MAX_SIZE  1000000
 
 
 int slowstart();
 int cogestionavoidance ();
 
 int next_seqno=0, send_base=0, exp_seqno, dup_ack, sockfd, serverlen, slow_cnt=0, slow_srt=0, count=0, end=0, temp_count;
-int ssh=5;
+int ssh=64;
 double cwnd=1;
 struct sockaddr_in serveraddr;
 struct itimerval timer;
 char buffer[DATA_SIZE]; 
 bool resend = false;
 bool func_resend = false;
-FILE *fp;
+FILE *fp, *hp;
 tcp_packet *sndpkt;
 tcp_packet *recvpkt;
 tcp_packet **temp_window;
-tcp_packet *window_pkt[256]; // a pointer array of all apckets in the current window
+tcp_packet *window_pkt[MAX_SIZE]; // a pointer array of all apckets in the current window
 sigset_t sigmask;
-int test=0;
+struct timeval tp;
 
 void resend_packets(int sig)//resends the window of packets 
 {   
@@ -58,7 +59,7 @@ void resend_packets(int sig)//resends the window of packets
             VLOG(DEBUG, "Resending packet %d to %s",temp_window[i]->hdr.seqno, inet_ntoa(serveraddr.sin_addr))
         }
     }else{
-        temp_window = (tcp_packet**) malloc(floor(cwnd)* DATA_SIZE);//creates a temprorary window to hold the packets being resent 
+        temp_window = (tcp_packet**) malloc((int)cwnd* DATA_SIZE);//creates a temprorary window to hold the packets being resent 
         if(temp_window == NULL)                     
         {
             printf("Error! memory not allocated.");
@@ -73,14 +74,15 @@ void resend_packets(int sig)//resends the window of packets
             temp_window[i] = window_pkt[i];
             VLOG(DEBUG, "Sending packet %d to %s",window_pkt[i]->hdr.seqno, inet_ntoa(serveraddr.sin_addr));
         }
+	//window_pkt[0]=window_pkt[0];
         //resets all the values so that fast retransmit can ocur from slow start
-        ssh=MAX(floor(cwnd),2);
-        temp_count = floor(cwnd);
+        ssh=MAX((int)cwnd,2);
+        temp_count = (int)cwnd;
         cwnd=1;
         dup_ack=0;
         slow_srt=0;
-        slow_cnt=0;
-        count=0;
+        slow_cnt=1;
+        count=1;
         //sets these to true so that packets will be continously be sent until an ack greater than them has arrived.
         resend = true;
         func_resend = true;
@@ -138,6 +140,10 @@ int main (int argc, char **argv)
         error(argv[3]);
     }
 
+    hp = fopen("CWND.csv", "a");
+    if (hp == NULL){
+        perror("Error opening file");
+    }
     /* socket: create the socket */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) 
@@ -163,7 +169,7 @@ int main (int argc, char **argv)
     //Stop and wait protocol
 
     init_timer(RETRY, resend_packets);
-
+    
     LOOP: do
     {
         if (slowstart() == 0){//ends if the file is completely sent in the slow start phase
@@ -186,7 +192,7 @@ int main (int argc, char **argv)
 
 int slowstart() {// initiates slow start at the beginning of a cycle 
     int len, num=0;
-    printf("slow start\n");
+    printf("slow start, %d, %d, %lf\n", slow_cnt, slow_srt, cwnd);
     while(slow_cnt < cwnd){
         len = fread(buffer, 1, DATA_SIZE, fp);
         if ( len <= 0)//checks if we've  reached the end of the file and breaks out of the for loop
@@ -223,9 +229,17 @@ int slowstart() {// initiates slow start at the beginning of a cycle
     }
 
     if (end >= count){//once all pakets in window have been ACKed and EOF reached, end program
+        fclose(fp);
+        fclose(hp);
         return 0;
     }
-    start_timer();
+
+    //documents the size of control window at a given moment in time
+    gettimeofday(&tp, NULL);
+    fprintf(hp,"%lu, %f\n",tp.tv_sec,cwnd);
+
+
+    start_timer();//starts timer for next packet to be acked 
     if(recvfrom(sockfd, buffer, MSS_SIZE, 0,
         (struct sockaddr *) &serveraddr, (socklen_t *)&serverlen) < 0)
     {
@@ -233,20 +247,22 @@ int slowstart() {// initiates slow start at the beginning of a cycle
     }
     recvpkt = (tcp_packet *)buffer;
     assert(get_data_size(recvpkt) <= DATA_SIZE);
-    stop_timer();
-
-    if(func_resend){//responsibole for exiting function so that fast retransmit can occur
-        func_resend = false;
-        return 1;
+    stop_timer();//stops the timer once an ack is received
+    
+    for (int i = 0; i < count; i++){
+      printf("packet %d in position %d, window size: %lf\n", window_pkt[i]->hdr.seqno, i, cwnd);
     }
-
     exp_seqno = window_pkt[0]->hdr.seqno + window_pkt[0]->hdr.data_size;//gets the expected sequence number for the ACK to be received
     if(exp_seqno <= recvpkt->hdr.ackno){//allows for cumulative ACKS
         int i;
+	printf("recv ack: %d\n",recvpkt->hdr.ackno);
         for (i = 0; i < count; i++){
             if ((window_pkt[i]->hdr.seqno + window_pkt[i]->hdr.data_size) == recvpkt->hdr.ackno){//determines which of the packets is being ACKed
                 num = i+1;
             }
+	    /*if ((window_pkt[i]->hdr.seqno + window_pkt[i]->hdr.data_size) > recvpkt->hdr.ackno){
+		num = i;
+	    }*/
             window_pkt[i-num] =  window_pkt[i];//makes window shift to the packets after the last ACKed
         }
         slow_srt = (i-num);//points to the new position in window where the next packets will go 
@@ -265,15 +281,14 @@ int slowstart() {// initiates slow start at the beginning of a cycle
             resend_packets(0);
         }
     }
-    printf("window size: %f\n",cwnd);
     return 1;
    
 }
 
 int cogestionavoidance () {//initatiates cogestion avoidancee
     int len, num=0;
-    printf("congestion avoidnce\n");
-    while(slow_cnt < floor(cwnd)){
+    printf("congestion avoidance, %d, %d, %lf\n", slow_cnt, slow_srt, cwnd);
+    while(slow_cnt < (int)cwnd){
         len = fread(buffer, 1, DATA_SIZE, fp);
         if ( len <= 0)//checks if we've  reached the end of the file and breaks out of the for loop
         {
@@ -286,9 +301,9 @@ int cogestionavoidance () {//initatiates cogestion avoidancee
         }
 
         if (end > 0){//allows  for the iteration of the window size ot to grow pass the EOF 
-            count = (floor(cwnd)-1);
+            count = ((int)cwnd-1);
         }else{
-            count=floor(cwnd);
+            count=(int)cwnd;
         }
 
         send_base = next_seqno;
@@ -309,8 +324,14 @@ int cogestionavoidance () {//initatiates cogestion avoidancee
     }
 
     if (end >= count){//once all pakets in window have been ACKed and EOF reached, end program
+        fclose(fp);
+        fclose(hp);
         return 0;
     }
+
+    //documents the size of control window at a given moment in time
+    gettimeofday(&tp, NULL);
+    fprintf(hp,"%lu, %f\n",tp.tv_sec,cwnd);
 
     start_timer();
     if(recvfrom(sockfd, buffer, MSS_SIZE, 0,
@@ -323,9 +344,8 @@ int cogestionavoidance () {//initatiates cogestion avoidancee
     stop_timer();
 
 
-    if(func_resend){//responsible for exiting functionso that fast restransmit can occur
-        func_resend = false;
-        return 1;
+    for (int i = 0; i < count; i++){
+      printf("packet %d in position %d, window size: %lf\n", window_pkt[i]->hdr.seqno, i, cwnd);
     }
 
     exp_seqno = window_pkt[0]->hdr.seqno + window_pkt[0]->hdr.data_size;//gets the expected sequence number for the ACK to be received
@@ -333,16 +353,20 @@ int cogestionavoidance () {//initatiates cogestion avoidancee
 
     if(exp_seqno <= recvpkt->hdr.ackno){//allows for cumulative ACKS
         int i;
+	printf("recv ack: %d\n",recvpkt->hdr.ackno);
         for (i = 0; i < count; i++){
             if ((window_pkt[i]->hdr.seqno + window_pkt[i]->hdr.data_size) == recvpkt->hdr.ackno){//determines which of the packets is being ACKed
                 num = i+1;
             }
+	 //    if ((window_pkt[i]->hdr.seqno + window_pkt[i]->hdr.data_size) > recvpkt->hdr.ackno){
+		// num = i;
+	 //    }
             window_pkt[i-num] =  window_pkt[i];//makes window shift to the packets after the last ACKed
         }
         slow_srt = (i-num);//points to the new position in window where the next packets will go 
         slow_cnt = slow_srt;//starts a ccount or the next number of packets and makes it not exceed the current window size
         if (end < 1){//does not increase window size once EOF is reached
-            cwnd = cwnd + ((1/floor(cwnd))*num);//increase window by a fraction and accounts for cummulative ACKS
+            cwnd = cwnd + ((1/(int)cwnd)*num);//increase window by a fraction and accounts for cummulative ACKS
         }
         dup_ack=0;//resets the duplicate ACK counter once the ACk has been recieved
         if (resend){//if the temproary window was created free the space in memory, and set the resedn value to false
@@ -355,6 +379,5 @@ int cogestionavoidance () {//initatiates cogestion avoidancee
             resend_packets(0);
         }
     }
-    printf("window size: %f\n",cwnd);
     return 1;
 }
