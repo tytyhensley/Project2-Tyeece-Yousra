d96f1594f8f10a85fae5b27f5f6a351623f09371
yelhassan <yeh218@nyu.edu>
2020-05-16 18:33:54 +0400

    Update rdt_sender.c
    Sender Update

--- rdt_sender.c
+++ rdt_sender.c
@@ -10,55 +10,82 @@
 #include <sys/time.h>
 #include <time.h>
 #include <assert.h>
+#include <math.h>
+#include <stdbool.h> 
 
 #include"packet.h"
 #include"common.h"
 
 #define STDIN_FD    0
 #define RETRY  120 //milli second 
-#define ssh 32
+#define MAX(a,b) ((a > b) ? a : b )
 
 
 int slowstart();
+int cogestionavoidance ();
 
-
-int next_seqno=0, send_base=0, exp_seqno, dup_ack, sockfd, serverlen, slow_cnt=0, slow_srt=0, cwnd=1, count=0, end=0;
+int next_seqno=0, send_base=0, exp_seqno, dup_ack, sockfd, serverlen, slow_cnt=0, slow_srt=0, count=0, end=0, temp_count;
+int ssh=5;
+double cwnd=1;
 struct sockaddr_in serveraddr;
 struct itimerval timer;
 char buffer[DATA_SIZE]; 
+bool resend = false;
+bool func_resend = false;
 FILE *fp;
 tcp_packet *sndpkt;
 tcp_packet *recvpkt;
-tcp_packet *window_pkt[ssh]; // a pointer array of all apckets in the current window
+tcp_packet **temp_window;
+tcp_packet *window_pkt[256]; // a pointer array of all apckets in the current window
 sigset_t sigmask;
+int test=0;
 
-void resend_packets(int sig)
-{
-    if (sig == SIGALRM)
-    {
-        //Resend all packets range between 
-        //sendBase and nextSeqNum
-        VLOG(INFO, "Timeout happend");
-        for (int i = 0; i<count; i++){
-            if(sendto(sockfd, window_pkt[i], TCP_HDR_SIZE + get_data_size(window_pkt[i]), 0, 
-                        ( const struct sockaddr *)&serveraddr, serverlen) < 0)
-            {
-                error("sendto");
-            }
-         VLOG(DEBUG, "Sending packet %d to %s",window_pkt[i]->hdr.seqno, inet_ntoa(serveraddr.sin_addr));
-        }
+void resend_packets(int sig)//resends the window of packets 
+{   
+    if (sig == SIGALRM){
+        VLOG(INFO, "Timeout happend"); 
+    }else{
+        VLOG(INFO, "Duplicate ACK happend");
     }
-    if(sig == 0){
-         VLOG(INFO, "Duplicate Ack happend");
-        for (int i = 0; i<count; i++){
+
+    if (resend) {
+        for (int i = 0; i<temp_count; i++){
+            if(sendto(sockfd, temp_window[i], TCP_HDR_SIZE + get_data_size(temp_window[i]), 0, 
+                    ( const struct sockaddr *)&serveraddr, serverlen) < 0)
+            {
+                error("sendto");
+            }
+            VLOG(DEBUG, "Resending packet %d to %s",temp_window[i]->hdr.seqno, inet_ntoa(serveraddr.sin_addr))
+        }
+    }else{
+        temp_window = (tcp_packet**) malloc(floor(cwnd)* DATA_SIZE);//creates a temprorary window to hold the packets being resent 
+        if(temp_window == NULL)                     
+        {
+            printf("Error! memory not allocated.");
+            exit(0);
+        }
+        for (int i = 0; i<count; i++){//resedns packet in window
             if(sendto(sockfd, window_pkt[i], TCP_HDR_SIZE + get_data_size(window_pkt[i]), 0, 
                         ( const struct sockaddr *)&serveraddr, serverlen) < 0)
             {
                 error("sendto");
             }
-         VLOG(DEBUG, "Sending packet %d to %s",window_pkt[i]->hdr.seqno, inet_ntoa(serveraddr.sin_addr));
+            temp_window[i] = window_pkt[i];
+            VLOG(DEBUG, "Sending packet %d to %s",window_pkt[i]->hdr.seqno, inet_ntoa(serveraddr.sin_addr));
         }
-    } 
+        //resets all the values so that fast retransmit can ocur from slow start
+        ssh=MAX(floor(cwnd),2);
+        temp_count = floor(cwnd);
+        cwnd=1;
+        dup_ack=0;
+        slow_srt=0;
+        slow_cnt=0;
+        count=0;
+        //sets these to true so that packets will be continously be sent until an ack greater than them has arrived.
+        resend = true;
+        func_resend = true;
+    }
+    
 }
 
 
@@ -136,21 +163,31 @@ int main (int argc, char **argv)
     //Stop and wait protocol
 
     init_timer(RETRY, resend_packets);
-    do
+
+    LOOP: do
     {
         if (slowstart() == 0){//ends if the file is completely sent in the slow start phase
             return 0;
         }
-
-    }while(cwnd != ssh);
-    printf("time for avoidance\n\n");
-    return 0;
-
+        if(resend){
+            goto LOOP;
+        }
+    }while(cwnd < ssh);
+    while(1){
+        if(cogestionavoidance() == 0){
+            return 0;
+        }
+        if(resend){
+            goto LOOP;
+        }
+    }
+    return 1;
 }
 
 int slowstart() {// initiates slow start at the beginning of a cycle 
     int len, num=0;
-    do{
+    printf("slow start\n");
+    while(slow_cnt < cwnd){
         len = fread(buffer, 1, DATA_SIZE, fp);
         if ( len <= 0)//checks if we've  reached the end of the file and breaks out of the for loop
         {
@@ -183,7 +220,93 @@ int slowstart() {// initiates slow start at the beginning of a cycle
             error("sendto");
         }
         slow_cnt+=1;
-    }while(slow_cnt < cwnd);
+    }
+
+    if (end >= count){//once all pakets in window have been ACKed and EOF reached, end program
+        return 0;
+    }
+    start_timer();
+    if(recvfrom(sockfd, buffer, MSS_SIZE, 0,
+        (struct sockaddr *) &serveraddr, (socklen_t *)&serverlen) < 0)
+    {
+        error("recvfrom");
+    }
+    recvpkt = (tcp_packet *)buffer;
+    assert(get_data_size(recvpkt) <= DATA_SIZE);
+    stop_timer();
+
+    if(func_resend){//responsibole for exiting function so that fast retransmit can occur
+        func_resend = false;
+        return 1;
+    }
+
+    exp_seqno = window_pkt[0]->hdr.seqno + window_pkt[0]->hdr.data_size;//gets the expected sequence number for the ACK to be received
+    if(exp_seqno <= recvpkt->hdr.ackno){//allows for cumulative ACKS
+        int i;
+        for (i = 0; i < count; i++){
+            if ((window_pkt[i]->hdr.seqno + window_pkt[i]->hdr.data_size) == recvpkt->hdr.ackno){//determines which of the packets is being ACKed
+                num = i+1;
+            }
+            window_pkt[i-num] =  window_pkt[i];//makes window shift to the packets after the last ACKed
+        }
+        slow_srt = (i-num);//points to the new position in window where the next packets will go 
+        slow_cnt = slow_srt;//starts a ccount or the next number of packets and makes it not exceed the current window size
+        if (end < 1){//does not increase window size once EOF is reached
+            cwnd++;
+        }
+        if (resend){//if the temproary window was created free the space in memory, and set the resedn value to false
+            free(temp_window);
+            resend=false;
+        }
+        dup_ack=0;//resets the duplicate ACK counter once the ACk has been recieved
+    }else{
+        dup_ack++;//accounts for duplicate ACKS
+        if (dup_ack == 3){
+            resend_packets(0);
+        }
+    }
+    printf("window size: %f\n",cwnd);
+    return 1;
+   
+}
+
+int cogestionavoidance () {//initatiates cogestion avoidancee
+    int len, num=0;
+    printf("congestion avoidnce\n");
+    while(slow_cnt < floor(cwnd)){
+        len = fread(buffer, 1, DATA_SIZE, fp);
+        if ( len <= 0)//checks if we've  reached the end of the file and breaks out of the for loop
+        {
+            end = end+1;//accumulates to keep track of when the EOF is reached and when all the packets in window have been acked.
+            VLOG(INFO, "End Of File has been reached");
+            sndpkt = make_packet(0);
+            sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
+                    (const struct sockaddr *)&serveraddr, serverlen);
+            break;
+        }
+
+        if (end > 0){//allows  for the iteration of the window size ot to grow pass the EOF 
+            count = (floor(cwnd)-1);
+        }else{
+            count=floor(cwnd);
+        }
+
+        send_base = next_seqno;
+        next_seqno = send_base + len;//creates the next sequence number 
+        sndpkt = make_packet(len);
+        memcpy(sndpkt->data, buffer, len);
+        sndpkt->hdr.seqno = send_base;
+
+        window_pkt[slow_cnt] = sndpkt;//adds the current send packet to the ith position in the window
+
+        VLOG(DEBUG, "Sending packet %d to %s",window_pkt[slow_cnt]->hdr.seqno, inet_ntoa(serveraddr.sin_addr));
+        if(sendto(sockfd, sndpkt, TCP_HDR_SIZE + get_data_size(sndpkt), 0, 
+                    ( const struct sockaddr *)&serveraddr, serverlen) < 0)
+        {
+            error("sendto");
+        }
+        slow_cnt+=1;
+    }
 
     if (end >= count){//once all pakets in window have been ACKed and EOF reached, end program
         return 0;
@@ -199,7 +322,14 @@ int slowstart() {// initiates slow start at the beginning of a cycle
     assert(get_data_size(recvpkt) <= DATA_SIZE);
     stop_timer();
 
+
+    if(func_resend){//responsible for exiting functionso that fast restransmit can occur
+        func_resend = false;
+        return 1;
+    }
+
     exp_seqno = window_pkt[0]->hdr.seqno + window_pkt[0]->hdr.data_size;//gets the expected sequence number for the ACK to be received
+
 
     if(exp_seqno <= recvpkt->hdr.ackno){//allows for cumulative ACKS
         int i;
@@ -212,16 +342,19 @@ int slowstart() {// initiates slow start at the beginning of a cycle
         slow_srt = (i-num);//points to the new position in window where the next packets will go 
         slow_cnt = slow_srt;//starts a ccount or the next number of packets and makes it not exceed the current window size
         if (end < 1){//does not increase window size once EOF is reached
-            cwnd++;
+            cwnd = cwnd + ((1/floor(cwnd))*num);//increase window by a fraction and accounts for cummulative ACKS
         }
         dup_ack=0;//resets the duplicate ACK counter once the ACk has been recieved
+        if (resend){//if the temproary window was created free the space in memory, and set the resedn value to false
+            free(temp_window);
+            resend=false;
+        }
     }else{
         dup_ack++;//accounts for duplicate ACKS
         if (dup_ack == 3){
             resend_packets(0);
         }
     }
+    printf("window size: %f\n",cwnd);
     return 1;
 }
-
-
